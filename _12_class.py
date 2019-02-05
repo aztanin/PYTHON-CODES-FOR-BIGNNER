8164c8a1624f3f8333aeab7ec15e2b74538e5b60
arifuzzaman tanin <arifuzzamantanin@gmail.com.com>
2019-02-06 00:01:16 +0600

    12 added | 06-02-2019

---
+++ _12_class.py
@@ -0,0 +1,8 @@
+class Account:
+    def __init__(self):
+        print("I'm self method")
+    def Meta(self,address):
+        print(address)
+
+account = Account()
+account.Meta("CHA 119/1, North Badda, Dhaka 1212")
