7bc170dba31834a38436726012a94635fcef16f6
arifuzzaman tanin <arifuzzamantanin@gmail.com.com>
2019-02-06 00:16:29 +0600

    13 INHERITANCE | 06-02-2019

---
+++ _13_inheritance.py
@@ -0,0 +1,18 @@
+class Education:
+    def Degree(self,degreeName):
+        print(degreeName)
+
+class Hobby:
+    def MyHobby(self,hobbyName):
+        print(hobbyName)
+
+class Account(Education, Hobby):
+    def __init__(self):
+        print("I'm self method")
+    def Meta(self,address):
+        print(address)
+
+account = Account()
+account.Meta("Dhaka, Bangladesh")
+account.Degree("B.Sc")
+account.MyHobby("I've any hobby")
